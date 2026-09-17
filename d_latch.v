7270ab969beadd89629acfd1774b2ce0d1c31b03
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-09-18 01:40:36 +0530

    Add files via upload

---
+++ d_latch.v
@@ -0,0 +1,24 @@
+module d_latch(
+input  enb,rst, d_in, output reg q, qbar
+    );
+    
+    always@(enb)
+    begin 
+    if(!enb)
+    if(rst)
+    begin
+    q<=1'b0;
+    qbar<=1'b1;
+    
+    end
+    
+    else begin 
+    q<= d_in;
+    qbar <= ~d_in;
+    end
+    
+    
+    end
+    
+    
+endmodule
---
+++ d_latch_tb.v
@@ -0,0 +1,31 @@
+module d_latch_tb(
+
+    );
+    
+    reg enb, d_in, rst;
+    wire q, qbar;
+    
+    d_latch dut( enb,rst,d_in,q,qbar);
+    
+    initial
+    begin
+    
+    {enb,rst,d_in}=0;
+
+    
+    end
+    always #5 enb = ~enb;
+    initial
+    begin
+    
+    rst =1;
+    #15;
+    rst =0;
+    #5
+    d_in = 1;
+    #10;
+    d_in =0;
+    
+    end
+
+endmodule
